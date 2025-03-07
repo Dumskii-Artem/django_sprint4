fcaf53a5a72cc305537ae117667d805695955bd4
Dumskii Artem <boltushko@gmail.com>
2025-03-07 06:52:57 +0300

    1-2

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -21,8 +21,9 @@ urlpatterns = [
          name='add_comment'),
     path('posts/<int:post_id>/edit_comment/<int:comment_id>/',
          views.edit_comment, name='edit_comment'),
+
     path('posts/<int:post_id>/delete_comment/<int:comment_id>/',
-         views.edit_comment, name='delete_comment'),
+         views.delete_comment, name='delete_comment'),
 
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/<slug:username>/', views.UserDetailView.as_view(),
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,9 +1,22 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class AboutView(TemplateView):
-    template_name = "pages/about.html"
+class AboutPage(TemplateView):
+    template_name = 'pages/about.html'
 
 
-class RulesView(TemplateView):
-    template_name = "pages/rules.html"
+class RulesPage(TemplateView):
+    template_name = 'pages/rules.html'
+
+
+def error404(request, exception=None):
+    return render(request, 'pages/404.html', status=404)
+
+
+def error403csrf(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def error500(request, exception=None):
+    return render(request, 'pages/500.html', status=500)
